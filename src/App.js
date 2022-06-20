985d59c1227ff154705a8caf0ae3e87c3475b6ac
hashfx <algebra.soni@gmail.com>
2022-06-20 14:41:54 +0530

    save to local storage

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ import { Footer } from './MyComponents/Footer';
 import React, { useState } from 'react';  // useState hook
 
 function App() {
+  // when the app is initialised, check for existing todos from the local storage
+  let initTodo;
+  if(localStorage.getItem("todos")){  // if NULL
+    initTodo = []  // initialise an empty array
+  }else{
+    initTodo = JSON.parse(localStorage.getItem("todos"))
+  }
   const onDelete = (todo) => {
     console.log("onDelete Clicked for: ", todo)
-    // deleting following way does not wor in React
+    // deleting following way does not work in React
     // let index = todos.indexOf(todo)
     // todos.splice(index, 1)  // deletes 1 item from the list
 
@@ -17,6 +24,7 @@ function App() {
     setTodos(todos.filter((e) => {
       return e !== todo;
     }))
+    localStorage.setItem("todos", JSON.stringify(todos))
   }
   // add todo with their title and desc to todos list
   const addTodo = (title, desc) => {
@@ -37,28 +45,35 @@ function App() {
     // update added todo to DOM
     setTodos([...todos, myTodo])  // add new todo to existing array of todos
     console.log(myTodo)  // display myTodo
+
+    // save todo to local storage as they are added
+      localStorage.setItem("todos", JSON.stringify(todos))  // set todos to "todos"
   }
 
+  /* default manually added todos
   const [todos, setTodos] = useState([  // setTodos is a function to update todos
-  
-  /*
-    {
-      sno: 1,
-      title: "Todo1",
-      desc: "Yay, First"
-    },
-    {
-      sno: 2,
-      title: "Todo 2",
-      desc: "Great"
-    },
-    {
-      sno: 3,
-      title: "Third Todo",
-      desc: "Do it"
-    }
-    */
+      {
+        sno: 1,
+        title: "Todo1",
+        desc: "Yay, First"
+      },
+      {
+        sno: 2,
+        title: "Todo 2",
+        desc: "Great"
+      },
+      {
+        sno: 3,
+        title: "Third Todo",
+        desc: "Do it"
+      }
   ]);
+  */
+
+  // using local storage to store and retrieve todos
+  // when the app is initialised
+  const [todos, setTodos] = useState([initTodo]);  // pass empty array to todos
+
   return (
     <>
       <Header title="Todos List App" searchBar={false} />
