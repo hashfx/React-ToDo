f14578b684bc0fb035f65229ae9b1f22418ecf81
hashfx <algebra.soni@gmail.com>
2022-06-20 18:00:31 +0530

    v6 router changed && not rendering home components

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ function App() {
       <Router>
         <Header title="Todos List" searchBar={false} />
         <Routes>
-          <Route exact path="/" render={() => {
+          <Route index path="/" render={() => {
             return (
               <>
                 <AddTodo AddTodo={addTodo} />
